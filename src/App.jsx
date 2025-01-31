d603165b92a7471e2076fcf8d5e99d3ea586c261
anvDT <devabvsng@gmail.com>
2025-01-31 19:37:48 +0530

    Accordion Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Accordion } from "./components/Accordion";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Accordion title={"Acc Title"}>
+        <div className="w-fit text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas sit
+          rerum accusamus cupiditate non corrupti temporibus eaque enim debitis
+          accusantium, veniam unde inventore omnis libero nemo odio quam eos
+          totam qui voluptate nostrum quibusdam placeat in molestiae? Voluptates
+          omnis totam ipsam mollitia commodi, aspernatur, necessitatibus,
+          officiis accusantium corrupti fugiat perspiciatis sapiente? Pariatur
+          possimus, labore quas dicta quaerat veniam non dolor debitis, autem
+          quisquam vel quos odio nemo odit molestias unde voluptates officia a
+          quia? Neque eum officiis dolorem velit provident suscipit vero
+          inventore dolores. Dicta architecto saepe distinctio quis a, libero
+          esse quas neque, at excepturi dolore eaque error suscipit?
+        </div>
+      </Accordion>
+    </>
+  );
 }
 
 export default App;
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,33 @@
-export function Accordion() {
-  return <></>;
+import { useState } from "react";
+
+/* eslint-disable react/prop-types */
+export function Accordion({ title, children }) {
+  const [showAccContent, setShowAccContent] = useState(false);
+  return (
+    <>
+      <div className="bg-black text-white p-3 w-1/5 m-3  rounded-3xl">
+        <AccTitle title={title} setShowAccContent={setShowAccContent} />
+        {showAccContent && <AccContent content={children} />}
+      </div>
+    </>
+  );
+}
+function AccTitle({ title, setShowAccContent }) {
+  return (
+    <div
+      onClick={() => {
+        setShowAccContent((p) => !p);
+      }}
+      className=" p-2 font-semibold text-xl cursor-pointer hover:bg-gray-700"
+    >
+      {title}
+    </div>
+  );
+}
+function AccContent({ content }) {
+  return (
+    <>
+      <div className="text-xs">{content}</div>
+    </>
+  );
 }
