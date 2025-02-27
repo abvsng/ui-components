e06d1170edeb5ed606ff57ee4de58cf1fb3f33dd
anvDT <devabvsng@gmail.com>
2025-02-27 19:27:28 +0530

    SearchBar Done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,10 @@ export function SearchBar() {
   return (
     <>
       <div className="flex flex-row relative">
-        <button className=" bg-slate-200 hover:bg-slate-300">
+        <button className=" bg-slate-300 hover:bg-slate-200 cursor-pointer rounded-full p-3 absolute left-0">
           <SearchIcon />
         </button>
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" className="pl-11 outline-none bg-slate-100 hover:bg-slate-200 rounded-full py-2 "/>
       </div>
     </>
   );
--- src/svg/SearchIcon.jsx
+++ src/svg/SearchIcon.jsx
@@ -7,7 +7,7 @@ export function SearchIcon() {
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="size-6"
+        className="size-4"
       >
         <path
           strokeLinecap="round"
